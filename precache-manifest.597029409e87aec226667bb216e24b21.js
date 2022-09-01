74461d497d1f733f9d580ca34adb56d9c33002d9
Code Sandbox <hello@codesandbox.io>
2022-09-01 11:54:48 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.597029409e87aec226667bb216e24b21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2d31232c5e653684675226dd0d5c30a",
+    "url": "https://ljubicaboneva.github.io/csb-twuryi/index.html"
+  },
+  {
+    "revision": "0c2d9b35013206274932",
+    "url": "https://ljubicaboneva.github.io/csb-twuryi/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "61baf496bcf0ebf8d0a4",
+    "url": "https://ljubicaboneva.github.io/csb-twuryi/static/js/2.e70f6a46.chunk.js"
+  },
+  {
+    "revision": "f9fd082ead9847e8580baac16ac64d23",
+    "url": "https://ljubicaboneva.github.io/csb-twuryi/static/js/2.e70f6a46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c2d9b35013206274932",
+    "url": "https://ljubicaboneva.github.io/csb-twuryi/static/js/main.d3518904.chunk.js"
+  },
+  {
+    "revision": "8370ab2e97489c1efdfe",
+    "url": "https://ljubicaboneva.github.io/csb-twuryi/static/js/runtime-main.2cbd55a5.js"
+  }
+]);
